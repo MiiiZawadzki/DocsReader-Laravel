1481822525ae71b60e5c0b13c197f48f6c289c67
MiiiZawadzki <zawadzki.micha@gmail.com>
2025-01-19 13:49:58 +0100

    feat: add permission config list & sync permissions command

---
+++ config/permissions.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | System permissions
+    |--------------------------------------------------------------------------
+    |
+    | This list is responsible for the basic scope of permissions within the application.
+    | All new permissions should be added to this list so that they can be synchronized when
+    | building the application.
+    |
+    */
+
+    'manage-documents',
+];
