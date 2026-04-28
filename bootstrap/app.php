ac1e3ec6f2605c87a9ddb8223ea496e1df422455
MiiiZawadzki <zawadzki.micha@gmail.com>
2026-04-28 17:16:24 +0200

    fix: trust proxy headers for HTTPS URL generation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->statefulApi();
         $middleware->prepend(SetLocale::class);
     })
