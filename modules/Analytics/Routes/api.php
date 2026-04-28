86816e4bb7a4af3a2a1607da892facf4c669e583
MiiiZawadzki <zawadzki.micha@gmail.com>
2026-04-28 17:17:04 +0200

    fix: manage group naming

--- modules/Analytics/Routes/api.php
+++ modules/Analytics/Routes/api.php
@@ -6,33 +6,31 @@ use Modules\Analytics\Http\Controllers\ManageStatisticsController;
 use Modules\Analytics\Http\Controllers\UserStatisticsController;
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::prefix('statistics')->name('statistics')->group(function () {
-        Route::prefix('user')->name('user')->group(function () {
-            Route::get('/stats', [UserStatisticsController::class, 'stats']);
-            Route::get('/documents/active', [UserStatisticsController::class, 'activeDocuments']);
-            Route::get('/documents/total', [UserStatisticsController::class, 'totalDocuments']);
-            Route::get('/documents/read', [UserStatisticsController::class, 'readDocuments']);
+    Route::prefix('statistics')->name('statistics.')->group(function () {
 
-
-            Route::get('/charts', [UserStatisticsController::class, 'charts']);
-            Route::get('/read', [UserStatisticsController::class, 'readStatistics']);
+        Route::prefix('user')->name('user.')->group(function () {
+            Route::get('/stats',            [UserStatisticsController::class, 'stats'])->name('stats');
+            Route::get('/documents/active', [UserStatisticsController::class, 'activeDocuments'])->name('documents.active');
+            Route::get('/documents/total',  [UserStatisticsController::class, 'totalDocuments'])->name('documents.total');
+            Route::get('/documents/read',   [UserStatisticsController::class, 'readDocuments'])->name('documents.read');
+            Route::get('/charts',           [UserStatisticsController::class, 'charts'])->name('charts');
+            Route::get('/read',             [UserStatisticsController::class, 'readStatistics'])->name('read');
         });
-        Route::prefix('manage')->name('manage')->group(function () {
-            Route::get('/stats', [ManageStatisticsController::class, 'stats']);
-            Route::get('/documents/active', [ManageStatisticsController::class, 'activeDocuments']);
-            Route::get('/documents/total', [ManageStatisticsController::class, 'totalDocuments']);
 
-            Route::get('/charts', [ManageStatisticsController::class, 'charts']);
-            Route::get('/read', [ManageStatisticsController::class, 'readStatistics']);
+        Route::prefix('manage')->name('manage.')->group(function () {
+            Route::get('/stats',            [ManageStatisticsController::class, 'stats'])->name('stats');
+            Route::get('/documents/active', [ManageStatisticsController::class, 'activeDocuments'])->name('documents.active');
+            Route::get('/documents/total',  [ManageStatisticsController::class, 'totalDocuments'])->name('documents.total');
+            Route::get('/charts',           [ManageStatisticsController::class, 'charts'])->name('charts');
+            Route::get('/read',             [ManageStatisticsController::class, 'readStatistics'])->name('read');
 
-            Route::prefix('document/{document}')->name('manage')->group(function () {
-                Route::get('/stats', [DocumentStatisticsController::class, 'stats']);
-                Route::get('/assigned', [DocumentStatisticsController::class, 'documentAssignment']);
-                Route::get('/reads', [DocumentStatisticsController::class, 'documentReads']);
-                Route::get('/ratio', [DocumentStatisticsController::class, 'documentReadRatio']);
-
-                Route::get('/charts', [DocumentStatisticsController::class, 'charts']);
-                Route::get('/read', [DocumentStatisticsController::class, 'readStatistics']);
+            Route::prefix('document/{document}')->name('document.')->group(function () {
+                Route::get('/stats',    [DocumentStatisticsController::class, 'stats'])->name('stats');
+                Route::get('/assigned', [DocumentStatisticsController::class, 'documentAssignment'])->name('assigned');
+                Route::get('/reads',    [DocumentStatisticsController::class, 'documentReads'])->name('reads');
+                Route::get('/ratio',    [DocumentStatisticsController::class, 'documentReadRatio'])->name('ratio');
+                Route::get('/charts',   [DocumentStatisticsController::class, 'charts'])->name('charts');
+                Route::get('/read',     [DocumentStatisticsController::class, 'readStatistics'])->name('read');
             });
         });
     });
